90ed0c5337ed816e3894cef62c58de6e415d8261
smallflyfish <ghiczelb231@gmail.com>
2017-04-09 23:19:46 +0800

    hw6

---
+++ hw6/hw6/Program.cs
@@ -0,0 +1,41 @@
+﻿using opennlp.tools.tokenize;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using opennlp.tools.sentdetect;
+
+namespace hw6
+{
+	class Program
+	{
+		static string[] tokens;
+		static void Main(string[] args)
+		{
+			String[] file = Directory.GetFiles(@"..\..\..\", "*.txt");
+			StreamWriter sw = new StreamWriter(@"..\..\..\output\Html.txt");
+			foreach (String files in file)
+			{
+				using (StreamReader sr = new StreamReader(files))
+				{
+					while (sr.Peek() != -1)
+					{
+						string line = sr.ReadLine();
+						int i;
+						java.io.InputStream modelin = new java.io.FileInputStream(string.Format(@"{0}\en-sent.bin", @"..\Debug"));
+						SentenceModel model = new SentenceModel(modelin);
+						SentenceDetector detector = new SentenceDetectorME(model);
+						string[] sents = detector.sentDetect(line);
+						foreach (var sent in sents)
+						{
+							sw.WriteLine(sent);
+						}
+
+
+					}
+				}
+				sw.Flush();
+			}
+			sw.Close();
+		}
+	}
+}
